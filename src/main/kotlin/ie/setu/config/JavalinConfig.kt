eb6d45b009c89ac21c59086b37fb5b1e79ba0aaf
Siobhan Drohan <sdrohan@wit.ie>
2024-05-15 15:43:16 +0100

    Lab04 - Before starting exercises

---
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -0,0 +1,25 @@
+package ie.setu.config
+
+import ie.setu.controllers.HealthTrackerController
+import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.*
+
+class JavalinConfig {
+
+    fun startJavalinService(): Javalin {
+
+        val app = Javalin.create().apply {
+            exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
+            error(404) { ctx -> ctx.json("404 - Not Found") }
+        }.start(7000)
+
+        registerRoutes(app)
+        return app
+    }
+
+    private fun registerRoutes(app: Javalin) {
+        app.get("/api/users", HealthTrackerController::getAllUsers)
+        app.get("/api/users/{user-id}", HealthTrackerController::getUserByUserId)
+        app.post("/api/users", HealthTrackerController::addUser)
+    }
+}
