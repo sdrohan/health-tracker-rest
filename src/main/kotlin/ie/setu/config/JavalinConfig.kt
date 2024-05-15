031446f54795b5ca4539dc2a92231d689b19c94d
Siobhan Drohan <sdrohan@wit.ie>
2024-05-15 16:21:52 +0100

    Lab04 - After finishing exercises

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -19,7 +19,11 @@ class JavalinConfig {
 
     private fun registerRoutes(app: Javalin) {
         app.get("/api/users", HealthTrackerController::getAllUsers)
+
         app.get("/api/users/{user-id}", HealthTrackerController::getUserByUserId)
-        app.post("/api/users", HealthTrackerController::addUser)
+        app.delete("/api/users/{user-id}", HealthTrackerController::deleteUser)
+        app.patch("/api/users/{user-id}", HealthTrackerController::updateUser)
+
+        app.get("/api/users/email/{email}", HealthTrackerController::getUserByEmail)
     }
 }
