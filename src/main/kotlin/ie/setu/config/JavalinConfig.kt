d7991f8b6bbf4969ee4a849f22defd5ae9eb1fe6
Siobhan Drohan <sdrohan@wit.ie>
2024-05-16 09:31:42 +0100

    Lab05 - add user crud functionality

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -19,8 +19,9 @@ class JavalinConfig {
 
     private fun registerRoutes(app: Javalin) {
         app.get("/api/users", HealthTrackerController::getAllUsers)
-
         app.get("/api/users/{user-id}", HealthTrackerController::getUserByUserId)
+
+        app.post("/api/users", HealthTrackerController::addUser)
         app.delete("/api/users/{user-id}", HealthTrackerController::deleteUser)
         app.patch("/api/users/{user-id}", HealthTrackerController::updateUser)
 
