9833adced03e338f6ca82ce0807ea3f0cfe344f6
Siobhan Drohan <sdrohan@wit.ie>
2024-10-17 22:26:32 +0100

    Vue - add bootstrap and nav bar

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -4,19 +4,20 @@ import ie.setu.controllers.HealthTrackerController
 import ie.setu.utils.jsonObjectMapper
 import io.javalin.Javalin
 import io.javalin.json.JavalinJackson
+import io.javalin.vue.VueComponent
 
 class JavalinConfig {
 
     fun startJavalinService(): Javalin {
-        val app = Javalin.create {
-            //add this jsonMapper to serialise objects to json
+        val app = Javalin.create{
+            //added this jsonMapper for our integration tests - serialise objects to json
             it.jsonMapper(JavalinJackson(jsonObjectMapper()))
-        }
-            .apply{
-                exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
-                error(404) { ctx -> ctx.json("404 - Not Found") }
-            }
-            .start(getRemoteAssignedPort())
+            it.staticFiles.enableWebjars()
+            it.vue.vueInstanceNameInJs = "app" // only required for Vue 3, is defined in layout.html
+        }.apply {
+            exception(Exception::class.java) { e, _ -> e.printStackTrace() }
+            error(404) { ctx -> ctx.json("404 : Not Found") }
+        }.start(getRemoteAssignedPort())
 
         registerRoutes(app)
         return app
@@ -47,6 +48,15 @@ class JavalinConfig {
         app.patch("/api/activities/{activity-id}", HealthTrackerController::updateActivity)
 
         app.get("/api/activities/{activity-id}", HealthTrackerController::getActivitiesByActivityId)
+
+
+        // The @routeComponent that we added in layout.html earlier will be replaced
+        // by the String inside the VueComponent. This means a call to / will load
+        // the layout and display our <home-page> component.
+        app.get("/", VueComponent("<home-page></home-page>"))
+        app.get("/users", VueComponent("<user-overview></user-overview>"))
+        app.get("/users/{user-id}", VueComponent("<user-profile></user-profile>"))
+        app.get("/users/{user-id}/activities", VueComponent("<user-activity-overview></user-activity-overview>"))
     }
 
     private fun getRemoteAssignedPort(): Int {
