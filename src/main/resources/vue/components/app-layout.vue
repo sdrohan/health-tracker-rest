e84bf716282164220be65e75636494789cedfc9e
Siobhan Drohan <sdrohan@wit.ie>
2024-10-18 11:58:38 +0100

    lab08 - update bootstrap to latest version

--- src/main/resources/vue/components/app-layout.vue
+++ src/main/resources/vue/components/app-layout.vue
@@ -4,18 +4,16 @@
       <!-- Start of navbar -->
       <nav class="navbar navbar-expand-lg navbar-light">
         <a class="navbar-brand" href="/">Home</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse"
-                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                 aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
+          <ul class="navbar-nav me-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="/users">
-                Users <span class="sr-only">(current)</span>
-              </a>
+              <a class="nav-link" href="/users">Users</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/activities">Activities</a>
@@ -40,7 +38,7 @@ app.component("app-layout", {template: "#app-layout"});
 </script>
 
 <style>
-.navbar{
+.navbar {
   background-color: #e3f2fd;
 }
 </style>
