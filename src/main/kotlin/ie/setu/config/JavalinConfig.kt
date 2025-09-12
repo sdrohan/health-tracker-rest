71d5fad4813f6d0beaa93fe9328844fbd07c2e74
Siobhan Drohan <sdrohan@wit.ie>
2025-09-12 15:48:16 +0100

    Prepare app for Openshift Deployment

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -2,31 +2,39 @@ package ie.setu.config
 
 import ie.setu.controllers.HealthTrackerController
 import io.javalin.Javalin
-import io.javalin.apibuilder.ApiBuilder.*
 
 class JavalinConfig {
 
+    val app = Javalin.create().apply {
+        exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
+        error(404) { ctx -> ctx.json("404 - Not Found") }
+    }
+
     fun startJavalinService(): Javalin {
-
-        val app = Javalin.create().apply {
-            exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
-            error(404) { ctx -> ctx.json("404 - Not Found") }
-        }.start(7001)
-
+        app.start(getRemoteAssignedPort())
         registerRoutes(app)
         return app
     }
 
     private fun registerRoutes(app: Javalin) {
         app.get("/api/users", HealthTrackerController::getAllUsers)
-
         app.get("/api/users/{user-id}", HealthTrackerController::getUserByUserId)
+        app.post("/api/users", HealthTrackerController::addUser)
         app.delete("/api/users/{user-id}", HealthTrackerController::deleteUser)
         app.patch("/api/users/{user-id}", HealthTrackerController::updateUser)
-
         app.get("/api/users/email/{email}", HealthTrackerController::getUserByEmail)
     }
 
+    private fun getRemoteAssignedPort(): Int {
+        val remotePort = System.getenv("PORT")
+        return if (remotePort != null) {
+            Integer.parseInt(remotePort)
+        } else 8080
+    }
 
+    fun getJavalinService(): Javalin {
+        registerRoutes(app)
+        return app
+    }
 
 }
