c5929699415ed74becaae79f6f97905b66f192d3
Siobhan Drohan <sdrohan@wit.ie>
2025-09-15 18:06:50 +0100

    Add Vue dependency and Vue landing page

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -4,18 +4,21 @@ import ie.setu.controllers.HealthTrackerController
 import ie.setu.utils.jsonObjectMapper
 import io.javalin.Javalin
 import io.javalin.json.JavalinJackson
+import io.javalin.vue.VueComponent
 
 class JavalinConfig {
 
     val app = Javalin.create(
         { config ->
             config.jsonMapper(JavalinJackson(jsonObjectMapper()))
+            config.staticFiles.enableWebjars()
+            config.vue.vueInstanceNameInJs = "app"
+
         }
     ).apply {
         exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
         error(404) { ctx -> ctx.json("404 - Not Found") }
     }
-
 
     fun startJavalinService(): Javalin {
         app.start(getRemoteAssignedPort())
@@ -44,6 +47,12 @@ class JavalinConfig {
         app.delete("/api/activities/{activity-id}", HealthTrackerController::deleteActivityByActivityId)
         app.patch("/api/activities/{activity-id}", HealthTrackerController::updateActivity)
         app.get("/api/activities/{activity-id}", HealthTrackerController::getActivitiesByActivityId)
+
+        // The @routeComponent that we added in layout.html earlier will be replaced
+        // by the String inside the VueComponent. This means a call to / will load
+        // the layout and display our <home-page> component.
+        app.get("/", VueComponent("<home-page></home-page>"))
+
     }
 
     private fun getRemoteAssignedPort(): Int {
