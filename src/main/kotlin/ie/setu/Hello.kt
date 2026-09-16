ff146dfb6d4fa5809bd902eb90c613756e795047
Siobhan Drohan <siobhan.drohan@setu.ie>
2026-09-16 17:44:08 +0100

    Initial commit

---
+++ src/main/kotlin/ie/setu/Hello.kt
@@ -0,0 +1,6 @@
+package ie.setu
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/ie/setu/HelloTest.kt
@@ -0,0 +1,8 @@
+package ie.setu
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
